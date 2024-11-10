ba420a490dc507fb0b252d7c66f5dae85e70b664
Varadraj Patil <161109529+Parallel-Script@users.noreply.github.com>
2024-11-10 20:59:19 +0530

    Update heroku_login_push.sh
    n

--- heroku_login_push.sh
+++ heroku_login_push.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
 
-# Change to the Jenkins workspace directory
-cd /var/lib/jenkins/workspace/spp10pipeline
-
-# Login to Heroku using expect
-/usr/bin/expect <<EOF
-spawn heroku login -i
-expect "Email:"
-send "veeruved186@gmail.com\r"
-expect "Password:"
-send "HRKU-7f8a55e4-d16a-44b4-8bfc-5d6d3c95987b\r"
-expect eof
-EOF
-
 # Push to Heroku
 git push heroku
 
